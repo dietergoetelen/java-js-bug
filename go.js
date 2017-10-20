ca6f4309986d59c5393a766801170105a00aeaa3
Dieter Goetelen <dieter.goetelen@hotmail.com>
2017-10-20 15:12:33 +0200

    I can write Rust & Go & Java

---
+++ go.js
@@ -0,0 +1,5 @@
+/* -*- Mode: Go; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+const foo = {
+    bar: 'booh'
+};
---
+++ rust.js
@@ -0,0 +1,5 @@
+/* -*- Mode: Rust; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+const foo = {
+    bar: 'booh'
+};
